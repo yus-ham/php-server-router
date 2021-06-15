4b96af1ac23cd11b09c049fd2f46820ad62a1ba4
Yusup Hambali <supalpuket@gmail.com>
2021-06-15 09:24:53 +0700

    support parsing .htaccess rewrite directive

--- router.php
+++ router.php
@@ -6,81 +6,169 @@
 
 class Config
 {
+  const debug = true;
   const protected_paths = '~/\.git(\/.*)?$|/nbproject~';
   const show_files = true;
 }
 
 
-class Router {
+class Router
+{
+  private static $docRoot;
+  private static $pathInfo;
+  private static $prevPathInfo;
+  private static $requestURI;
+  private static $rewriteURI;
+  private static $parsedHtaccess = [];
 
   // default = index.php,index.html
   // @see [getScripts()]
-  static $scripts;
+  public static $scripts;
 
-  public static function setup() {
+  public static function setup()
+  {
     $port = ($_SERVER['SERVER_PORT'] != '80') ? ":$_SERVER[SERVER_PORT]" : "";
     $_SERVER['SERVER_ADDR'] = "$_SERVER[SERVER_NAME]$port";
+    self::setRequestURI();
   }
 
-  public static function run() {
+  public static function run()
+  {
     self::setup();
+    self::maybeOnlyJsRequest();
+    return self::serveURI();
+  }
 
-    if (self::isJsRequest()) {
-      exit(self::sendJs());
-    }
+  protected static function serveURI()
+  {
+    $currentUri = self::$requestURI;
+    error_log("REQUEST_URI = $currentUri");
 
-    $reqUrl = self::getRequestUrl();
-    error_log("[router] REQUEST_URI = $reqUrl");
-
-    if (self::isProtected($reqUrl)) {
+    if (self::isProtected($currentUri)) {
       http_response_code(403);
       self::showError('HTTP/1.1 403 Forbidden');
     }
 
-    $webroot = str_replace('\\', '/', $_SERVER['DOCUMENT_ROOT']);
-    $path = $webroot . $reqUrl;
+    self::$docRoot = str_replace('\\', '/', $_SERVER['DOCUMENT_ROOT']);
+    $path = self::$docRoot . $currentUri;
+    constant('Config::debug') && error_log('DOCUMENT_ROOT = ' . self::$docRoot);
 
     if (is_dir($path)) {
-      return self::serveDir($path);
+      return self::serveDir($path, $currentUri);
+    }
+    if (is_file($path)) {
+      if (self::isDotPHP($path)) {
+        constant('Config::debug') && error_log(print_r(__METHOD__ . ' ' . $path, 1));
+        $_SERVER['SCRIPT_NAME'] = $currentUri;
+        $_SERVER['PATH_INFO'] = self::$pathInfo;
+        $_SERVER['PHP_SELF'] = $currentUri . self::$prevPathInfo . self::$pathInfo;
+        self::includeScript($path);
+      }
+      return false;
     }
 
-    if (!is_file($path)) {
-      $i = 0;
-      do {
-        $reqUrl = str_replace('\\', '/', dirname($reqUrl));
-        $reqUrl = rtrim($reqUrl, '/');
-        $dir = $webroot . $reqUrl;
+    $i = 0;
+    do {
+      $currentUri = rtrim(str_replace('\\', '/', dirname($currentUri)), '/');
+      $dir = self::$docRoot . $currentUri;
 
-        if (false === self::serveIndex($dir)) {
-          return;
-        }
-      } while (($i++ < 20) && ($reqUrl && $reqUrl !== '/'));
-    }
+      if (false === self::serveIndex($dir, $currentUri)) {
+        return;
+      }
+    } while (($i++ < 20) && ($currentUri && $currentUri !== '/'));
   }
 
-  protected static function getScripts() {
+  protected static function getScripts()
+  {
     if (!self::$scripts) {
       self::$scripts = array('index.php', 'index.html');
 
       if ($altScripts = getenv('ALT_SCRIPT')) {
         self::$scripts = array_merge(explode(',', $altScripts), self::$scripts);
       }
+
+      array_unshift(self::$scripts, '.htaccess');
     }
     return self::$scripts;
   }
 
-  protected static function serveIndex($dir) {
+  protected static function serveIndex($dir, $currentUri)
+  {
     foreach (self::getScripts() as $script) {
-      $script = $dir .'/'. $script;
-      error_log("[router] trying script: $script");
+      $script = $dir . '/' . $script;
+      constant('Config::debug') && error_log("trying script: $script");
       if (is_file($script)) {
-        return self::serveScript($script, $dir);
+        constant('Config::debug') && error_log("Found script: $script");
+        if (self::isHtaccess($script)) {
+          if (self::serveHtaccess($script, $dir, $currentUri) === null) {
+            continue;
+          }
+        }
+        return self::serveScript($script, $dir, $currentUri);
       }
     }
   }
 
-  protected static function serveDir($dir) {
-    if (false === self::serveIndex($dir)) {
+  protected static function isHtaccess($file)
+  {
+    return strpos($file, '/.htaccess') !== false;
+  }
+
+  protected static function serveHtaccess($file, $dir, $currentUri)
+  {
+    if (in_array($file, self::$parsedHtaccess)) {
+      return;
+    }
+    self::$parsedHtaccess[] = $file;
+    constant('Config::debug') && error_log(__METHOD__);
+    $stopParsing = false;
+
+    foreach (file($file) as $line) {
+      if ($stopParsing) {
+        return;
+      }
+      @list($command, $args) = explode(' ', trim($line), 2);
+      constant('Config::debug') && error_log(__METHOD__ . ' ' . print_r([$command, $args], 1));
+
+      if (!$command or strpos($command, 'Rewrite') === false) {
+        continue;
+      }
+      $args = preg_split('/ +/', trim($args));
+      if ($command === 'RewriteEngine' && strtolower($args[0]) === 'on') {
+        self::$rewriteURI = true;
+        continue;
+      }
+      if (!self::$rewriteURI) {
+        throw new \Exception('Rewrite engine is off');
+      }
+      if ($command === 'RewriteCond') {
+        if ($args[0] === '%{REQUEST_FILENAME}') {
+          if ($args[1] === '!-d' && is_dir(self::$docRoot . self::$requestURI)) {
+            return;
+          }
+          if ($args[1] === '!-f' && is_file(self::$docRoot . self::$requestURI)) {
+            return;
+          }
+        }
+      }
+      if ($command === 'RewriteRule') {
+        $newURI = preg_replace('/' . $args[0] . '/', explode('?', $args[1])[0], ltrim(self::$pathInfo, '/'));
+        self::$pathInfo = substr(self::$requestURI, strlen($currentUri));
+        self::$prevPathInfo = $newURI;
+        error_log(__METHOD__ . ' ' . __LINE__ . ' ' . print_r([$newURI, $dir, $currentUri], 1));
+        self::$requestURI = $currentUri . '/' . $newURI;
+
+        return self::serveURI();
+      }
+    }
+    return true;
+  }
+
+  protected static function serveDir($dir, $currentUri)
+  {
+    constant('Config::debug') && error_log(__METHOD__ . ' ' . print_r(func_get_args(), 1));
+    $dir = rtrim($dir, '/');
+    if (false === self::serveIndex($dir, $currentUri)) {
       return;
     }
     http_response_code(404);
@@ -89,28 +177,46 @@ class Router {
     }
   }
 
-  protected static function serveScript($script, $dir) {
-    error_log("[router] SCRIPT_FILENAME = $script");
+  protected static function serveScript($script, $dir, $currentUri)
+  {
+    constant('Config::debug') && error_log(__METHOD__ . ' ' . print_r(func_get_args(), 1));
+    constant('Config::debug') && error_log(print_r($_SERVER, 1));
+    constant('Config::debug') && error_log(print_r(['pathInfo' => self::$pathInfo], 1));
+    error_log(__METHOD__ . " SCRIPT_FILENAME = $script");
 
-    // PHP fails to serve path that contains dot
-    $hasDot = strpos($dir, '.') !== false;
-    $isPHP = pathinfo($script, PATHINFO_EXTENSION) === 'php';
-    if (!$hasDot && $isPHP) {
+    // PHP Built-in server fails to serve path that contains dot
+    $hasDotInDir = strpos($dir, '.') !== false;
+    if (!$hasDotInDir && self::isDotPHP($script) && !self::$prevPathInfo) {
       return false;
     }
-    chdir($dir);
-    $_SERVER['SCRIPT_NAME'] .= $script;
-    $_SERVER['PHP_SELF'] = $_SERVER['SCRIPT_NAME'];
+
+    if (self::$pathInfo) {
+      $_SERVER['SCRIPT_NAME'] = substr($script, strlen(self::$docRoot));
+      $_SERVER['PHP_SELF'] = $_SERVER['SCRIPT_NAME'] . self::$pathInfo;
+    } else {
+      $_SERVER['SCRIPT_NAME'] .= $script;
+      $_SERVER['PHP_SELF'] = $_SERVER['SCRIPT_NAME'];
+    }
+
+    self::includeScript($script);
+  }
+
+  protected static function includeScript($script)
+  {
+    chdir(dirname($script));
     include $_SERVER['SCRIPT_FILENAME'] = $script;
+    constant('Config::debug') && error_log("Script included: $script");
     exit();
   }
 
-  protected static function getRequestUrl() {
+  protected static function setRequestURI()
+  {
     $exploded = explode('?', $_SERVER['REQUEST_URI'], 2);
-    return rtrim($exploded[0], '/');
+    return self::$requestURI = rtrim($exploded[0], '/');
   }
 
-  protected static function showFiles($dir) {
+  protected static function showFiles($dir)
+  {
     header('Content-Type: text/html');
     $files = array_merge((array) @scandir($dir), []);
     sort($files);
@@ -118,13 +224,13 @@ class Router {
               <style>body{font: normal 1.4em/1.4em monospace}
               a{text-decoration:none} a:hover{background:#B8C7FF}</style></head><body>';
 
-    $reqUrl = self::getRequestUrl();
+    $reqUri = self::$requestURI;
 
     foreach ($files as $file) {
       if ($file === '.') {
         continue;
       }
-      $link = "$reqUrl/$file/";
+      $link = "$reqUri/$file/";
       if (is_dir("$dir/$file")) {
         echo "<div class=row>[&bull;] <a href='$link'>$file/</a></div>\n";
       } else {
@@ -133,16 +239,17 @@ class Router {
     }
 
     foreach ((array) @$_files as $file) {
-      $link = "$reqUrl/$file";
+      $link = "$reqUri/$file";
       $bytes = filesize($dir . '/' . $file);
       echo "[&bull;] <a href='$link'>$file</a> (<span class=filesize>$bytes</span>)<br/>\n";
     }
 
     $time = filemtime(__FILE__);
-    echo "<script src=/?$time.js></script></body></html>";
+    echo "<script src='/?$time.js'></script></body></html>";
   }
 
-  protected static function showError($message) {
+  protected static function showError($message)
+  {
     $template = "<html><meta name='viewport' content='width=device-width, initial-scale=1'>
                 <title>$message</title><body>
                 <p><code>>> $_SERVER[REQUEST_METHOD] " . htmlspecialchars(urldecode($_SERVER['REQUEST_URI'])) . " $_SERVER[SERVER_PROTOCOL]</code></p>
@@ -150,23 +257,31 @@ class Router {
     exit($template);
   }
 
-  protected static function isProtected($path) {
+  protected static function isProtected($path)
+  {
     $regex = Config::protected_paths;
     if (preg_match($regex, $path)) {
       return true;
     }
   }
 
-  protected static function isJsRequest() {
-    return $_SERVER['REQUEST_URI'] === "/?".filemtime(__FILE__).".js";
+  protected static function maybeOnlyJsRequest()
+  {
+    if ($_SERVER['REQUEST_URI'] !== '/?' . filemtime(__FILE__) . '.js') {
+      return;
+    }
+
+    header('Content-Type: application/javascript');
+    header('Cache-Control: public, max-age=' . strtotime('6 month'));
+    echo "// link: http://stackoverflow.com/a/20463021"
+      . "\nfileSizeIEC = (a,b,c,d,e) => (b=Math,c=b.log,d=1024,e=c(a)/c(d)|0,a/b.pow(d,e)).toFixed(2) +' '+(e?'KMGTPEZY'[--e]+'iB':'Bytes')"
+      . "\ndocument.querySelectorAll('.filesize').forEach((e) => e.innerHTML = fileSizeIEC(e.innerHTML))";
+    die;
   }
 
-  protected static function sendJs() {
-    header('Content-Type: application/javascript');
-    header('Cache-Control: public, max-age='. strtotime('6 month'));
-    echo "// link: http://stackoverflow.com/a/20463021"
-       . "\nfileSizeIEC = (a,b,c,d,e) => (b=Math,c=b.log,d=1024,e=c(a)/c(d)|0,a/b.pow(d,e)).toFixed(2) +' '+(e?'KMGTPEZY'[--e]+'iB':'Bytes')"
-       . "\ndocument.querySelectorAll('.filesize').forEach((e) => e.innerHTML = fileSizeIEC(e.innerHTML))";
+  public static function isDotPHP($file)
+  {
+    return pathinfo($file, PATHINFO_EXTENSION) === 'php';
   }
 }
 
